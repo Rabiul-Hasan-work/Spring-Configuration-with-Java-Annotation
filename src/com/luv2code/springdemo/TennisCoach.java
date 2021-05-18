45dd337d19bf2278193ba6bc767aed307fdaa868
Rabiul Hasan <rabiulworkapps@gmail.com>
2021-05-18 14:57:02 +0600

    Field Injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
-
+	
+	@Autowired
 	private FortuneService fortuneService;
 	
 	// define a default constructor
@@ -13,12 +14,13 @@ public class TennisCoach implements Coach {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
 	
+	/*
 	@Autowired
 	public void doSomeCrazyStuff(FortuneService theFortuneService) {
 		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
 		fortuneService = theFortuneService;
 	}
-	
+	*/
 	/*
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
