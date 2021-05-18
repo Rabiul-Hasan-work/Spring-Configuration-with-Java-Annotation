fab821f23a5798b543fd7631d42816229547e2a8
Rabiul Hasan <rabiulworkapps@gmail.com>
2021-05-18 17:49:28 +0600

    injecting Values from Properties File

--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,11 +1,25 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+	
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getTeam() {
+		return team;
 	}
 	@Override
 	public String getDailyWorkout() {
